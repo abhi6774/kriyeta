31cffe04bde8f5e17e641e4d738573608ccd19bb
Anshul  choure <139856005+Anshul-251204@users.noreply.github.com>
2024-02-16 16:06:23 +0530

    add coustom Error & Response

--- apps/server/src/utils/apiResponse.ts
+++ apps/server/src/utils/apiResponse.ts
@@ -1,3 +1,12 @@
 class apiResponse {
-    
+    data:any;
+    message:string;
+    success: boolean
+    constructor(data:any, message:string){
+        this.data = data;
+        this.message = message;
+        this.success = true
+    }
 }
+
+export default apiResponse;
